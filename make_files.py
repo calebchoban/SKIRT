40ddb9a6aa7960111da7072d679d740c869ee7e1
Caleb Choban <calebchoban@gmail.com>
2022-10-04 11:51:16 -0700

    Added ability to specify binned wavelengths when seeting up files for SKIRT runs. Also, particles are now orientated to be face-on with galactic rotation vectors. This makes sure the default instrument position is looking at the galaxy face-on

--- make_files.py
+++ make_files.py
@@ -1,29 +1,30 @@
 
 import numpy as np
 import xml.etree.ElementTree as ET
-import copy
 import os
 from crc_scripts.gizmo import *
-
 from gadget_lib.load_stellar_hsml import get_particle_hsml
 
 # write particle file
 def write_particle_file(sdir, snum, odir, id=-1, cosmological=1, includeVels=True, importDust=True,
-                        separateDust=False, hdir=None, PAH_frac = 0.046):
-    sp = load_snap(sdir, snum, cosmological=cosmological)
+                        separateDust=False, hdir=None, PAHfrac=0.14, centerDisk=False):
 
-    hl = sp.loadhalo(id=id,hdir=hdir)
-    xc, yc, zc, rvir = hl.xc, hl.yc, hl.zc, hl.rvir
-    
-    p4 = sp.loadpart(4)
-    x, y, z = p4.p[:,0]-xc, p4.p[:,1]-yc, p4.p[:,2]-zc
+    #sp = load_snap(sdir, snum, cosmological=cosmological)
+    #hl = sp.loadhalo(id=id,hdir=hdir)
+    #xc, yc, zc, rvir = hl.xc, hl.yc, hl.zc, hl.rvir
+
+    halo = load_halo(sdir, snum, cosmological=cosmological, id=id, mode='AHF', hdir=hdir)
+    # Try and orientate halo so that it is face-on with the galaxy
+    print("Orientating halo to be face-on with galaxy....")
+    halo.set_orientation()
+
+    p4 = halo.loadpart(4)
+    x, y, z = p4.p[:,0], p4.p[:,1], p4.p[:,2]
     vx, vy, vz = p4.v[:,0], p4.v[:,1], p4.v[:,2]
     h = get_particle_hsml(x, y, z)
-    r = np.sqrt(x**2+y**2+z**2)
     m, Z, t = 1e10*p4.m, p4.z[:,0], 1e9*p4.age
 
     f = open(odir+"/star.dat", 'w')
-    j, = np.where(r<rvir)
     if includeVels:
         header =   '# star.dat \n' + \
                    '# Column 1: position x (pc)\n' + \
@@ -37,7 +38,7 @@ def write_particle_file(sdir, snum, odir, id=-1, cosmological=1, includeVels=Tru
                    '# Column 9: metallicity (1)\n' + \
                    '# Column 10: age (yr)\n'
         f.write(header)
-        for i in j:
+        for i in range(p4.npart):
             line = "%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.3e %.3e %.3e\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],vx[i],vy[i],vz[i],m[i],Z[i],t[i])
             f.write(line)
         f.close()
@@ -51,73 +52,88 @@ def write_particle_file(sdir, snum, odir, id=-1, cosmological=1, includeVels=Tru
                    '# Column 6: metallicity (mass fraction)\n' + \
                    '# Column 7: age (yr)\n'
         f.write(header)
-        for i in j:
+        for i in range(p4.npart):
             line = "%.2f %.2f %.2f %.2f %.3e %.3e %.3e\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],m[i],Z[i],t[i])
             f.write(line)
-        f.close()        
+        f.close()
 
     print("Star data written to star.dat...")
 
-    p0 = sp.loadpart(0)
-    x, y, z = p0.p[:,0]-xc, p0.p[:,1]-yc, p0.p[:,2]-zc
+    p0 = halo.loadpart(0)
+    x, y, z = p0.p[:,0], p0.p[:,1], p0.p[:,2]
     vx, vy, vz = p0.v[:,0], p0.v[:,1], p0.v[:,2]
     # If the snapshots include dust amounts, give those to SKIRT and set DZ to 1
     # Else just assume a constant DZ everywhere
     if importDust:
         h, m, T = p0.h, 1.0e10*p0.m*p0.dz[:,0], p0.T
         if separateDust:
-            max_PAH = 0.15 # Set maximum fraction of carbonceous dust which is PAHs
             silm = 1.0e10*p0.m*(p0.spec[:,0]+np.sum(p0.spec[:,2:],axis=1)) # Assume everything not carbonaceous is silicates
             graphm =1.0e10*p0.m*p0.spec[:,1]
-            PAHm = graphm*PAH_frac
+            PAHm = graphm*PAHfrac
             graphm -= PAHm
     else:
-        h, m, T = p0.h, 1.0e10*p0.m*p0.z[:,0], p0.T
-    r = np.sqrt(x**2+y**2+z**2)
+        h, m, Z, T = p0.h, 1.0e10*p0.m, p0.z[:,0], p0.T
 
     f = open(odir+"/gas.dat", 'w')
-    j, = np.where(r<rvir)
     # Make header for gas/dust. Needs to be in this specific order
     header =   '# gas.dat \n' + \
                '# Column 1: position x (pc)\n' + \
                '# Column 2: position y (pc)\n' + \
                '# Column 3: position z (pc)\n' + \
                '# Column 4: smoothing length (pc)\n'
-    if separateDust:
-            header += '# Column 5: silicate mass (Msun)\n' + \
-                      '# Column 6: graphite mass (Msun)\n' + \
-                      '# Column 7: PAH mass (Msun)\n' + \
-                      '# Column 8: temperature (K)\n'
-            if includeVels:
-                header +=  '# Column 9: velocity x (km/s)\n' + \
-                           '# Column 10: velocity y (km/s)\n' + \
-                           '# Column 11: velocity z (km/s)\n'
+    if separateDust and importDust:
+        header += '# Column 5: silicate mass (Msun)\n' + \
+                  '# Column 6: graphite mass (Msun)\n' + \
+                  '# Column 7: PAH mass (Msun)\n' + \
+                  '# Column 8: temperature (K)\n'
+        if includeVels:
+            header +=  '# Column 9: velocity x (km/s)\n' + \
+                       '# Column 10: velocity y (km/s)\n' + \
+                       '# Column 11: velocity z (km/s)\n'
+    elif importDust:
+        header += '# Column 5: dust mass (Msun)\n' + \
+                  '# Column 6: temperature (K)\n'
+        if includeVels:
+            header +=  '# Column 7: velocity x (km/s)\n' + \
+                       '# Column 8: velocity y (km/s)\n' + \
+                       '# Column 9: velocity z (km/s)\n'
     else:
-            header += '# Column 5: dust mass (Msun)\n' + \
-                      '# Column 6: temperature (K)\n'
-            if includeVels:
-                header +=  '# Column 7: velocity x (km/s)\n' + \
-                           '# Column 8: velocity y (km/s)\n' + \
-                           '# Column 9: velocity z (km/s)\n'
+        header += '# Column 5: mass (Msun)\n' + \
+                  '# Column 6: metallicity (1)\n' + \
+                  '# Column 7: temperature (K)\n'
+        if includeVels:
+            header +=  '# Column 8: velocity x (km/s)\n' + \
+                       '# Column 9: velocity y (km/s)\n' + \
+                       '# Column 10: velocity z (km/s)\n'
+
     f.write(header)
 
     if includeVels:
         if importDust and separateDust:
-            for i in j:
+            for i in range(p0.npart):
                 line = "%.2f %.2f %.2f %.3e %.3e %.3e %.3e %.3e %.2f %.2f %.2f\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],silm[i],graphm[i],PAHm[i],T[i],vx[i],vy[i],vz[i])
                 f.write(line)
-        else:
-            for i in j:
+        elif importDust:
+            for i in range(p0.npart):
                 line = "%.2f %.2f %.2f %.3e %.3e %.3e %.2f %.2f %.2f\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],m[i],T[i],vx[i],vy[i],vz[i])
+                f.write(line)
+        else:
+            for i in range(p0.npart):
+                line = "%.2f %.2f %.2f %.3e %.3e %.3e %.3e %.2f %.2f %.2f\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],m[i],Z[i],T[i],vx[i],vy[i],vz[i])
                 f.write(line)
         f.close()
     else:
         if importDust and separateDust:
+            for i in range(p0.npart):
                 line = "%.2f %.2f %.2f %.3e %.3e %.3e %.3e %.3e\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],silm[i],graphm[i],PAHm[i],T[i])
                 f.write(line)
-        else:
-            for i in j:
+        elif importDust:
+            for i in range(p0.npart):
                 line = "%.2f %.2f %.2f %.3e %.3e %.3e\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],m[i],T[i])
+                f.write(line)
+        else:
+            for i in range(p0.npart):
+                line = "%.2f %.2f %.2f %.3e %.3e %.3e %.3e\n" %(1e3*x[i],1e3*y[i],1e3*z[i],1e3*h[i],m[i],Z[i],T[i])
                 f.write(line)
         f.close()
 
@@ -127,9 +143,10 @@ def write_particle_file(sdir, snum, odir, id=-1, cosmological=1, includeVels=Tru
 
 # create parameter file
 def write_param_file(fname, dusttype='MW', includeCMB=False, includeVels=True, importDust=True, seperateDust=True,
-                     redshift=0.0, hubble=0.702,OmegaMatter=0.3175, DZ=1., gridsize=2e4, maxT=0, LTE=False):
+                     redshift=0.0, hubble=0.702,OmegaMatter=0.3175, DZ=1., gridsize=2e4, maxT=0, LTE=False,
+                     minWavelength=0.08, maxWavelength=1E3, wavebins=90):
 
-    if not seperateDust:
+    if not seperateDust or not importDust:
         infile = "sample/Sample_%s.ski"%dusttype
     else:
         infile = "sample/Sample_Multi.ski"
@@ -178,9 +195,10 @@ def write_param_file(fname, dusttype='MW', includeCMB=False, includeVels=True, i
         eleStellar.set('importVelocity', 'false')
         eleGas.set('importVelocity', 'false')
 
-    # set dust-to-metal ratio
+    # set dust-to-metal ratio if not importing dust
     if not importDust:
         eleGas.set('massFraction', "%.2f"%DZ)
+        eleGas.set('importMetallicity',"true")
     if maxT > 0:
         if importDust and seperateDust:
             eleSil.set('maxTemperature', "%.1e K"%maxT)
@@ -198,12 +216,42 @@ def write_param_file(fname, dusttype='MW', includeCMB=False, includeVels=True, i
     eleOctree.set('maxY', "%.2f pc"%gridsize)
     eleOctree.set('minZ', "-%.2f pc"%gridsize)
     eleOctree.set('maxZ', "%.2f pc"%gridsize)
+
+    # set source/emission wavelength distributions to match instrument
+    # This sets the source wavelength range
+    eleSourceWave = root[0][3][0]
+    eleSourceWave.set('minWavelength','%.3f micron'%minWavelength)
+    eleSourceWave.set('maxWavelength','%.1f micron'%maxWavelength)
+    # This sets the radiation field wavelength range
+    eleRadWave = root[0][4][0][1][0][0][0]
+    eleRadWave.set('minWavelength','%.3f micron'%minWavelength)
+    eleRadWave.set('maxWavelength','%.1f micron'%maxWavelength)
+    eleRadWave.set('numWavelengths','%i'%wavebins)
+    # This sets the emission wavelength range
+    eleEmissWave = root[0][4][0][4][0][1][0]
+    eleEmissWave.set('minWavelength','%.3f micron'%minWavelength)
+    eleEmissWave.set('maxWavelength','%.1f micron'%maxWavelength)
+    eleEmissWave.set('numWavelengths','%i'%wavebins)
+
     tree.write(fname, encoding='UTF-8', xml_declaration=True)
 
     return
 
+
+# Creates the log-spaces wavelength file for the mock observation instrument
+def write_wavelength_file(odir, minWavelength=0.08, maxWavelength=1E3, wavebins=90):
+    f = open(odir+"/wave.dat", 'w')
+    wavelengths = np.logspace(np.log10(minWavelength),np.log10(maxWavelength),wavebins)
+    f.write("%i\n"%wavebins)
+    for wavelength in wavelengths:
+        f.write('%f\n'%wavelength)
+    f.close()
+
+    return
+
 def make_files(sdir, odir, snum, id=-1, dusttype='MW', DZ=1, includeCMB=False, includeVels=True,
-               importDust=True, separateDust=True, cosmological=True, hdir=None, maxT=0, LTE=False, PAH_frac=0.046):
+               importDust=True, separateDust=True, cosmological=True, hdir=None, maxT=0, LTE=False, PAHfrac=0.14,
+               minWavelength=0.08, maxWavelength=1E3, wavebins=90, centerDisk=False):
 
     # read snapshot
     sp = load_snap(sdir, snum, cosmological=1)
@@ -217,13 +265,15 @@ def make_files(sdir, odir, snum, id=-1, dusttype='MW', DZ=1, includeCMB=False, i
     if importDust:
         DZ=1.
 
+    write_wavelength_file(odir, minWavelength=minWavelength, maxWavelength=maxWavelength, wavebins=wavebins)
     # write parameter file
     write_param_file(odir+"/out.ski", dusttype=dusttype, includeCMB=includeCMB, DZ=DZ, includeVels=includeVels,
-        importDust=importDust, seperateDust=separateDust, redshift=sp.redshift, gridsize=1e3*hl.rvir, maxT=maxT, LTE=LTE)
+                    importDust=importDust, seperateDust=separateDust, redshift=sp.redshift, gridsize=1e3*hl.rvir,
+                    maxT=maxT, LTE=LTE,minWavelength=minWavelength, maxWavelength=maxWavelength, wavebins=wavebins)
     os.system("cp wave.dat %s"%odir)
 
     # write particle file
     write_particle_file(sdir, snum, odir, id=id, includeVels=includeVels, importDust=importDust,
-                        separateDust=separateDust, hdir=hdir, PAH_frac=PAH_frac)
+                        separateDust=separateDust, hdir=hdir, PAHfrac=PAHfrac, centerDisk=centerDisk)
 
     return
--- run.py
+++ run.py
@@ -4,34 +4,52 @@ from make_files import *
 sdir = './scripts'
 sdir = '/oasis/tscc/scratch/cchoban/FIRE2_wind_tests/extended_AGB_winds/output/'
 # Output directory for SKIRT
-odir = './output'
+odir = './assumed_dust_output'
 # AHF halo directory
 hdir ='./halo'
 hdir='/oasis/tscc/scratch/cchoban/FIRE2_wind_tests/extended_AGB_winds/AHF_data/AHF_output/'
 # Number of snapshot
 snum = 600
 # Is the snapshot cosmological?
-cosmological=1
+cosmological = 1
 # ID of halo to use (-1 is most massive fully resolved halo)
 id = -1
 # Do you want to include relative particle velocities for SKIRT
 includeVels = True
 # Use this to set a constant dust-to-metals ratio (this is ignored if you import dust)
-DZ=0.5
+DZ = 0.5
 # If simulation used 'live' dust evolution you can import the tracked dust mass instead of assuming a constant DZ
-importDust = True
+importDust = False
 # Set dust composition for either assumed DZ or only importing total dust mass (MW, SMC)
 dustType = 'MW'
 # Do you want to instead separate total dust mass into separate species masses? (this supersedes dustType)
-separateDust=True
+separateDust = True
 # Mass fraction of carbonacous dust mass in PAHs, needed for separateDust since we don't explicitly track PAHs
 # For the MW qPAH=4.6% (fraction of total dust mass) which for 2-to-1 silicate to carbonaceous dust mass gives 14% of the total carbonaceous dust mass
 PAH_frac = 0.14
 # Max gas temperature to allow dust (0 K allows all gas to have dust)
-maxT=0
+maxT = 1E5
 # Set whether dust is assumed to be in LTE or non-LTE (default)
-LTE=False
+LTE = False
+# Determine the range of log-space wavelength grid for you mock observations
+minWavelength = 0.08
+maxWavelength = 1E3
+wavebins = 200
 
 
-make_files(sdir, odir, snum, id=id, dusttype=dustType, DZ=1., includeVels=includeVels, importDust=importDust,
+make_files(sdir, odir, snum, id=id, dusttype=dustType, DZ=DZ, includeVels=includeVels, importDust=importDust,
+		   cosmological=cosmological, hdir=hdir, separateDust=separateDust, maxT=maxT, LTE=LTE, PAH_frac=PAH_frac)
+
+odir = './import_separate_dust'
+maxT=0
+importDust=True
+separateDust=True
+make_files(sdir, odir, snum, id=id, dusttype=dustType, DZ=DZ, includeVels=includeVels, importDust=importDust,
+		   cosmological=cosmological, hdir=hdir, separateDust=separateDust, maxT=maxT, LTE=LTE, PAH_frac=PAH_frac)
+
+odir = './import_DZ_dust'
+maxT=0
+importDust=True
+separateDust=False
+make_files(sdir, odir, snum, id=id, dusttype=dustType, DZ=DZ, includeVels=includeVels, importDust=importDust,
 		   cosmological=cosmological, hdir=hdir, separateDust=separateDust, maxT=maxT, LTE=LTE, PAH_frac=PAH_frac)
