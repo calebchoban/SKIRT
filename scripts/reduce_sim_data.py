91160b41e86632bc24e79cd751edf35b084662b8
Caleb Choban <calebchoban@gmail.com>
2024-05-22 18:17:22 -0400

    Fixed some bugs in the data reduction

--- scripts/reduce_sim_data.py
+++ scripts/reduce_sim_data.py
@@ -20,31 +20,35 @@ def create_SKIRT_particle_files(snap_dir, snap_num, output_dir, importDust=True)
     importDust : boolean
         Sets whether dust information will be extracted from simulation.
     '''
-
     # This loads the galactic halo from the snapshot
     halo = load_halo(snap_dir, snap_num, mode='AHF')
     # This orientates the halo so that the galactic disk is face-on
     print("Orientating halo")
     halo.set_orientation()
 
+    if not halo.sp.Flag_DustSpecies and importDust:
+        print("WARNING: importDust set to True but this snapshot does not have dust. Set importDust to False.")
+
     # Load data for star particles (ptype = 4)
     p4 = halo.loadpart(4)
+    # x,y,x coordinates
     x, y, z = p4.p[:,0], p4.p[:,1], p4.p[:,2]
     # Compute the star softening lengths. This takes some time.
     print("Calculating star particle smoothing lengths...")
     h = get_particle_hsml(x, y, z)
+    # mass, metallicity, and age
     m, Z, t = 1e10*p4.m, p4.z[:,0], 1e9*p4.age
 
     f = open(output_dir+"/star.dat", 'w')
     # Write header for star file
-    header =   '# star.dat \n' + \
+    header =    '# star.dat \n' + \
                 '# Column 1: position x (pc)\n' + \
                 '# Column 2: position y (pc)\n' + \
                 '# Column 3: position z (pc)\n' + \
                 '# Column 4: smoothing length (pc)\n' + \
-                '# Column 8: mass (Msun)\n' + \
-                '# Column 9: metallicity (1)\n' + \
-                '# Column 10: age (yr)\n'
+                '# Column 5: mass (Msun)\n' + \
+                '# Column 6: metallicity (1)\n' + \
+                '# Column 7: age (yr)\n'
     f.write(header)
     # Step through each star particle and write its data
     for i in range(p4.npart):
@@ -56,12 +60,15 @@ def create_SKIRT_particle_files(snap_dir, snap_num, output_dir, importDust=True)
 
     # Load gas particle data (ptype = 0)
     p0 = halo.loadpart(0)
+    # x,y,x coordinates
     x, y, z = p0.p[:,0], p0.p[:,1], p0.p[:,2]
     # If the snapshots include dust amounts, give those to SKIRT and set D/Z to 1
     # Else just assume a constant D/Z everywhere.
     if importDust:
+        # smoothing length, mass, and temperature
         h, m, T = p0.h, 1.0e10*p0.m*p0.dz[:,0], p0.T
     else:
+        # smoothing length, mass, and temperature
         h, m, Z, T = p0.h, 1.0e10*p0.m, p0.z[:,0], p0.T
 
     f = open(output_dir+"/gas.dat", 'w')
