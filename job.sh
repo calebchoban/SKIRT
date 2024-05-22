57aafbcd3535c643f06ed9c1ec92818489a5c0d4
Caleb Choban <calebchoban@gmail.com>
2024-05-22 16:42:49 -0400

    Updated job submission script to run on Big Red 200

--- job.sh
+++ job.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
-#SBATCH -J SKIRT          # Job name
-#SBATCH -o out.o           # Name of stdout output file
-#SBATCH -e out.e           # Name of stderr error file
-#SBATCH -p normal          # Queue (partition) name
-#SBATCH -N 1              # Total # of nodes 
-#SBATCH --ntasks-per-node=32             # Total # of mpi tasks per node = # of Cores per Node (128) / OMP_NUM_THREADS
-#SBATCH --cpus-per-task=4  # of cpus per mpi task = OMP_NUM_THREADS
-#SBATCH -t 02:00:00        # Run time (hh:mm:ss)
-#SBATCH --mail-type=all    # Send email at begin and end of job
-#SBATCH -A AST21010        # Project/Allocation name (req'd if you have more than 1)
-#SBATCH --mail-user=EMAIL
+#SBATCH -J SKIRT            # Job name
+#SBATCH -o out.o            # Name of stdout output file
+#SBATCH -e out.e            # Name of stderr error file
+#SBATCH -p general          # Queue (partition) name
+#SBATCH --nodes=1           # Total # of nodes 
+#SBATCH --ntasks-per-node=4 # Total number of mpi tasks per node
+#SBATCH --cpus-per-task=8   # number of cpus per mpi task = OMP_NUM_THREADS.
+#SBATCH --mem=50G           # Total RAM to be used by this job
+#SBATCH -t 01:00:00         # Run time (hh:mm:ss)
+#SBATCH --mail-type=all     # Send email at begin and end of job
+#SBATCH -A r00380           # Project/Allocation name
+#SBATCH --mail-user=EMAIL   # Email to send all jobs alters to
 #SBATCH -D .
-#SBATCH --dependency=
 
+# Note that there are a total of 128 cores per node so (ntasks-per-node) * (cpus-per-task) should always be less than or equal to 128.
+# Also the max RAM is 256 GB.
 
-pwd
+export OMP_NUM_THREADS=4
 date
-source ./activate.sh
+module list
+
+# Make a new folder for the output and copy all the files SKIRT uses into it
+mkdir output
+cp out.ski ./output/
+cp star.dat ./output/
+cp gas.dat ./output/
 cd output
-# Run with 6 task each with 6 cores. 
-ibrun -cpus-per-task=$SLURM_CPUS_PER_TASK --ntasks-per-node=$SLURM_NTASKS_PER_NODE --nodes=$SLURM_JOB_NUM_NODES -m -v skirt out.ski
+pwd
+
+# Print out the SLURM info
+echo $SLURM_JOB_NUM_NODES
+echo $SLURM_NTASKS_PER_NODE
+echo $SLURM_CPUS_PER_TASK
+
+
+# Run with (ntasks-per-node) tasks each with (cpus-per-task) cores. 
+# skirt is a little odd in that you need to give the number of cores/threads using the -t argument to skirt and not to srun
+srun --ntasks-per-node=$SLURM_NTASKS_PER_NODE skirt -t $SLURM_CPUS_PER_TASK out.ski
 date
 exit
