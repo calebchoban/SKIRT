f8150946ca7c3e08c783581b9e5485b9da4fc398
Caleb Choban <calebchoban@gmail.com>
2026-02-10 16:26:17 -0500

    Got rid of old files

--- old_files/analyze.py
+++
@@ -1,8 +0,0 @@
-from plots import *
-
-dirc = "/oasis/tscc/scratch/cchoban/SKIRT_ver_comparison/SKIRT8/"
-inst_file = "out_i00_sed.dat"
-
-SED_plot(dirc, inst_file, out_dirc='./', img_name="SED.png", components=['total','transparent','stellar','dust_emission'], SKIRT_ver=8)
-
-calc_IRXBeta(dirc, inst_file)
